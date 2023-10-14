0f80dbcfb6395def7498458089f8eb69b254b877
jiho <19wlgh95@naver.com>
2023-10-14 20:05:47 +0900

    update

---
+++ progrmmers/인덱스바꾸기/sol.py
@@ -0,0 +1,19 @@
+# # 문자열 my_string과 정수 num1, num2가 매개변수로 주어질 때, my_string에서 인덱스 num1과 인덱스 num2에 
+# # 해당하는 문자를 바꾼 문자열을 return 하도록 solution 함수를 완성해보세요.
+
+def solution(my_string, num1, num2):
+    my_string = list(my_string)
+    my_string[num1], my_string[num2] = my_string[num2], my_string[num1]
+    answer = ''.join(my_string)
+    return answer
+
+
+
+
+# # my_string	num1	num2	result
+# # "hello"	1	2	"hlelo"
+# # "I love you"	3	6	"I l veoyou"
+
+print(solution('hello', 1, 2))
+print(solution('I love you', 3, 6))
+
