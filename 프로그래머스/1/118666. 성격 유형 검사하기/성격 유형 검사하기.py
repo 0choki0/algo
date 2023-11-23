73e2200f3e6c1f83f045a5ebb53f490aa1b72dbc
jiho_lee <145324322+0choki0@users.noreply.github.com>
2023-11-23 13:36:40 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.20 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,19 +1,30 @@
 def solution(survey, choices):
+    # 각 유형마다 점수를 주기 위해 dictionary 생성 
     char = {'R':0, 'T':0, 'C':0, 'F':0, 'J':0, 'M':0, 'A':0, 'N':0,}
+    
+    # 점수 부여
     for i in range(len(choices)):
+        # 비동의, 모르겠음의 경우
         if choices[i] <= 4:
-            f = 4-choices[i]
-            char[survey[i][0]] += f
+            # 부여하는 점수
+            score = 4-choices[i]
+            # survey의 앞 유형
+            char[survey[i][0]] += score
+        # 동의의 경우
         else:
-            b = choices[i]-4
-            char[survey[i][1]] += b
-
+            # 부여하는 점수
+            score = choices[i]-4
+            # survey의 뒷 유형
+            char[survey[i][1]] += score
+    
+    # 점수를 기준으로 각각의 유형 판별
+    # dict는 순서가 없기에 순서 역할을 할 key를 만들어줌
     new_char = {}
     for i, (key, value) in enumerate(char.items()):
         new_char[i] = (key, value)
     
     answer = ''
-    for i in range(0, 8, 2):
+    for i in range(0, len(char.keys()), 2):
         if new_char[i][1] >= new_char[i+1][1]:
             answer += new_char[i][0]
         else:
