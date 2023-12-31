4d7205971d84f48d327c9b3748509d639b6e0343
jiho_lee <145324322+0choki0@users.noreply.github.com>
2023-12-31 09:48:49 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,15 @@
+def solution(n,a,b):
+    answer = 1
+    while abs(a-b) != 1 or max([a,b]) % 2 != 0:
+        if a % 2 == 1:
+            a = (a + 1) / 2
+        else:
+            a = a / 2
+        
+        if b % 2 == 1:
+            b =  (b + 1) / 2
+        else:
+            b = b / 2
+        answer += 1
+
+    return answer
