1c2cd08a8c4ebe452be3ccfba25407a1150dbeab
jiho_lee <145324322+0choki0@users.noreply.github.com>
2024-01-18 10:14:41 +0900

    [level 2] Title: 타겟 넘버, Time: 2.94 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,10 @@
+def solution(numbers, target):
+    idx, curr = 0, 0
+    def dfs(nums, target, curr, idx):
+        if idx == len(nums):
+            return 1 if curr == target else 0
+        return dfs(nums, target, curr+nums[idx], idx+1) + dfs(nums, target, curr-nums[idx], idx+1)
+        
+    answer = dfs(numbers, target, idx, curr)
+    return answer
+        
