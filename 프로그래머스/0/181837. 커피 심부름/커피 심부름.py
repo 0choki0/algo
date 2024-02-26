f2f474f0975f065697f4054a7e3a844d01cac4a7
jiho_lee <145324322+0choki0@users.noreply.github.com>
2024-02-26 22:24:23 +0900

    [level 0] Title: 커피 심부름, Time: 0.00 ms, Memory: 9.97 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,10 @@
+def solution(order):
+    answer = 0
+    for ord in order:
+        if 'americano' in ord:
+            answer += 4500
+        elif 'latte' in ord:
+            answer += 5000
+        else:
+            answer += 4500
+    return answer
