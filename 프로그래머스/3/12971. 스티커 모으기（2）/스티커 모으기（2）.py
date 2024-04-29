4e6e00b920d5f522bfa9b87e495685e501f41302
jiho_lee <145324322+0choki0@users.noreply.github.com>
2024-04-29 20:27:32 +0900

    [level 3] Title: 스티커 모으기(2), Time: 62.59 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -0,0 +1,15 @@
+def solution(sticker):
+    answer = 0
+    size = len(sticker)
+    if size == 1:
+        return sticker.pop()
+    
+    dp1 = [0] + sticker[:-1]
+    for i in range(2, size):
+        dp1[i] = max(dp1[i-1], dp1[i-2]+dp1[i])
+
+    dp2 = [0] + sticker[1:]
+    for i in range(2, size):
+        dp2[i] = max(dp2[i-1], dp2[i-2]+dp2[i])
+
+    return max(dp1[-1], dp2[-1])
