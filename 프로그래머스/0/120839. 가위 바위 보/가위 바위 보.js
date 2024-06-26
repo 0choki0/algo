bf9735b641c254c14e75eab558611188e5a27bdf
jiho_lee <145324322+0choki0@users.noreply.github.com>
2024-06-26 10:37:55 +0900

    [level 0] Title: 가위 바위 보, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,13 @@
+function solution(rsp) {
+    var answer = '';
+    for (a of rsp) {
+        if (a==2) {
+            answer += 0;
+        } else if (a==0) {
+            answer += 5;
+        } else {
+            answer += 2
+        }
+    }
+    return answer;
+}
