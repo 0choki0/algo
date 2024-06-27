c29f59332923a7df0d99111f5a264cc8399da955
jiho_lee <145324322+0choki0@users.noreply.github.com>
2024-06-27 12:49:26 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
@@ -0,0 +1,9 @@
+function solution(arr, delete_list) {
+    let answer=[];
+    for (num of arr) {
+        if (!delete_list.includes(num)) {
+            answer.push(num);
+        }
+    }
+    return answer
+}
